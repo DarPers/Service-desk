20e70c14337295152eca7bd5d891df59bcb8a341
DarPers <104174389+DarPers@users.noreply.github.com>
2024-02-12 17:51:39 +0300

    Feature/add entities (#2)
    * feat: add gitignore
    
    * add entities and enums
    
    * change entities
    
    * add props to ticket entity, change Role enum
    
    * change admin to administrator in Role.cs
    
    * add base entity, change access modifiers

---
+++ ServiceDesk.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceDesk.DAL.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ ServiceDesk.DAL/Entities/ExecutionRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceDesk.DAL.Entities;
+
+public class ExecutionRequest : BaseEntity
+{
+    public Guid TicketId { get; set; }
+
+    public Ticket Ticket { get; set; } = null!;
+
+    public Guid ExecutorId { get; set; }
+
+    public User Executor { get; set; } = null!;
+}
