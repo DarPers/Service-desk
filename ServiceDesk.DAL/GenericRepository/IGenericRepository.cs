822864b10c007dfc12dce80868d1db39d53784dd
DarPers <104174389+DarPers@users.noreply.github.com>
2024-02-13 17:52:34 +0300

    Feature/add generic repository (#20)
    * feat: add gitignore
    
    * add generic repository
    
    * simplify DI for the repository
    
    * change access modifiers
    
    * fix namespace

---
+++ ServiceDesk.DAL/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using ServiceDesk.DAL.Entities;
+
+namespace ServiceDesk.DAL.GenericRepository;
+
+public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+
+    Task<TEntity?> GetEntityByPredicateAsync(Func<TEntity, bool> predicate, CancellationToken cancellationToken);
+
+    Task AddEntityAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task<IEnumerable<TEntity>> GetEntitiesByPredicateAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+    Task UpdateEntityAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task DeleteEntityAsync(TEntity entity, CancellationToken cancellationToken);
+
+    IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties);
+
+    IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+        params Expression<Func<TEntity, object>>[] includeProperties);
+}
