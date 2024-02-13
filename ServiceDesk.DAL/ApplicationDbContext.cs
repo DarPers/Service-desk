9c892148274faac80aeb25332b9b694189c51a93
DarPers <dariaerohova21@gmail.com>
2024-02-13 10:20:50 +0100

    add ApplicationDbContext and migration

---
+++ ServiceDesk.DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceDesk.DAL.Entities;
+
+namespace ServiceDesk.DAL.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+            
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Ticket> Tickets { get; set; }
+
+    public DbSet<ExecutionRequest> ExecutionRequests { get; set; }
+}
---
+++ ServiceDesk.DAL/DI/DependencyRegister.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceDesk.DAL.Data;
+
+namespace ServiceDesk.DAL.DI;
+
+public static class DependencyRegister
+{
+    public static IServiceCollection AddDataAccessLevelServices(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<ApplicationDbContext>(option =>
+            option.UseNpgsql(configuration.GetConnectionString("PostgresSQLConnectionString")));
+
+        return serviceCollection;
+    }
+}
