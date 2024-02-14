0db37b76db128367b8f440ccadd9eca30560e42d
DarPers <dariaerohova21@gmail.com>
2024-02-14 09:23:19 +0100

    add bisness logic layer

---
+++ ServiceDesk.BLL/DI/DependencyRegisterBll.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceDesk.DAL.DI;
+
+namespace ServiceDesk.BLL.DI;
+public static class DependencyRegisterBll
+{
+    public static IServiceCollection AddBusinessLogicLevelServices(this IServiceCollection serviceCollection, 
+        IConfiguration configuration)
+    {
+        serviceCollection.AddDataAccessLevelServices(configuration);
+
+        return serviceCollection;
+    }
+}
