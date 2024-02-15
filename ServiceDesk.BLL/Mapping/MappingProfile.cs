df992c0245151628ebccf91214a36853b455f6b3
DarPers <104174389+DarPers@users.noreply.github.com>
2024-02-15 10:45:09 +0300

    add mapping for models and entities (#24)
    * add mapping for models and entities
    
    * rename bll models

---
+++ ServiceDesk.BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ServiceDesk.BLL.Models;
+using ServiceDesk.DAL.Entities;
+
+namespace ServiceDesk.BLL.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, UserModel>().ReverseMap();
+        CreateMap<Ticket, TicketModel>().ReverseMap();
+        CreateMap<ExecutionRequest, ExecutionRequestModel>().ReverseMap();
+    }
+}
---
+++ ServiceDesk.BLL/Models/ExecutionRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceDesk.BLL.Models;
+
+public class ExecutionRequestModel : BaseModel
+{
+    public Guid TicketId { get; set; }
+
+    public TicketModel TicketModel { get; set; } = null!;
+
+    public Guid ExecutorId { get; set; }
+
+    public UserModel Executor { get; set; } = null!;
+}
