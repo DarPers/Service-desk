5b3a67ecb8bdc5c408874a0d0f17b363b8e59051
DarPers <dariaerohova21@gmail.com>
2024-02-19 11:51:15 +0100

    add DI for services

--- ServiceDesk.BLL/Services/ExecutionRequestService.cs
+++ ServiceDesk.BLL/Services/ExecutionRequestService.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
+using ServiceDesk.BLL.Interfaces;
 using ServiceDesk.BLL.Models;
 using ServiceDesk.DAL.Entities;
 using ServiceDesk.DAL.GenericRepository;
 
 namespace ServiceDesk.BLL.Services;
 
-public class ExecutionRequestService : GenericService<ExecutionRequestModel, ExecutionRequest>
+public class ExecutionRequestService : GenericService<ExecutionRequestModel, ExecutionRequest>, IExecutionRequestService
 {
     public ExecutionRequestService(IGenericRepository<ExecutionRequest> executionRequestRepository, IMapper mapper) 
         : base(executionRequestRepository, mapper)
