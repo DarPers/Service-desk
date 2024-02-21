a49f507659cb840205829f1095ed0fced9d6e2ad
DarPers <dariaerohova21@gmail.com>
2024-02-21 08:58:10 +0100

    change returned type in controller

--- ServiceDeskAPI/Controllers/TicketsController.cs
+++ ServiceDeskAPI/Controllers/TicketsController.cs
@@ -7,63 +7,57 @@ using ServiceDeskAPI.ViewModels;
 
 namespace ServiceDeskAPI.Controllers
 {
-    [Route("api/tickets")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TicketsController : ControllerBase
     {
         private readonly ITicketService _ticketService;
         private readonly IMapper _mapper;
 
-        TicketsController(ITicketService ticketService, IMapper mapper)
+        public TicketsController(ITicketService ticketService, IMapper mapper)
         {
             _ticketService = ticketService;
             _mapper = mapper;
         }
 
-        [HttpGet("/all")]
-        public async Task<IActionResult> GetAllTickets(CancellationToken cancellationToken)
+        [HttpGet("all")]
+        public async Task<IEnumerable<TicketViewModel>> GetAllTickets(CancellationToken cancellationToken)
         {
             var ticketModels = await _ticketService.GetAllAsync(cancellationToken);
-            var ticketViewModels = _mapper.Map<IEnumerable<TicketViewModel>>(ticketModels);
-            return Ok(ticketViewModels);
+            return _mapper.Map<IEnumerable<TicketViewModel>>(ticketModels);
         }
 
-        [HttpGet("/users/{id:Guid}")]
-        public async Task<IActionResult> GetTicketsByUserId(Guid id, CancellationToken cancellationToken)
+        [HttpGet("users/{id:Guid}")]
+        public async Task<IEnumerable<TicketViewModel>> GetTicketsByUserId(Guid id, CancellationToken cancellationToken)
         {
             var ticketModels = await _ticketService.GetListByPredicateAsync(p => p.UserId == id, cancellationToken);
-            var ticketViewModels = _mapper.Map<TicketViewModel>(ticketModels);
-            return Ok(ticketViewModels);
+            return _mapper.Map<IEnumerable<TicketViewModel>>(ticketModels);
         }
 
-        [HttpDelete("/{id:Guid}")]
-        public async Task<IActionResult> DeleteTicket(Guid id, CancellationToken cancellationToken)
+        [HttpDelete("{id:Guid}")]
+        public async Task DeleteTicket(Guid id, CancellationToken cancellationToken)
         {
             await _ticketService.DeleteModelAsync(id, cancellationToken);
-            return Ok();
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateTicket(TicketViewModel ticket, CancellationToken cancellationToken)
+        public async Task UpdateTicket(TicketViewModel ticket, CancellationToken cancellationToken)
         {
             var ticketModel = _mapper.Map<TicketModel>(ticket);
             await _ticketService.UpdateModelAsync(ticketModel, cancellationToken);
-            return Ok();
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateTicket(TicketCreationViewModel ticket, CancellationToken cancellationToken)
+        public async Task CreateTicket(TicketCreationViewModel ticket, CancellationToken cancellationToken)
         {
             var ticketModel = _mapper.Map<TicketModel>(ticket);
             await _ticketService.CreateModelAsync(ticketModel, cancellationToken);
-            return Ok();
         }
 
         [HttpPatch("{id:Guid}/status")]
-        public async Task<IActionResult> UpdateTicketStatus(Guid id, Status status, CancellationToken cancellationToken)
+        public async Task UpdateTicketStatus(Guid id, Status status, CancellationToken cancellationToken)
         {
             await _ticketService.SetTicketStatus(id, status, cancellationToken);
-            return Ok();
         }
     }
 }
