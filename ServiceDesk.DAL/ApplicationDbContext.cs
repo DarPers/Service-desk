d6ea2b359b1dfb1a88cf62e33bd9cab995b10e47
DarPers <dariaerohova21@gmail.com>
2024-03-01 14:31:42 +0100

    add auto calculation for createdAt and updatedAt datetimes

--- ServiceDesk.DAL/ApplicationDbContext.cs
+++ ServiceDesk.DAL/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-            
     }
 
     public DbSet<User> Users { get; set; }
@@ -14,4 +13,30 @@ public class ApplicationDbContext : DbContext
     public DbSet<Ticket> Tickets { get; set; }
 
     public DbSet<ExecutionRequest> ExecutionRequests { get; set; }
+
+    public override int SaveChanges()
+    {
+        var modifiedEntities = ChangeTracker.Entries()
+            .Where(i => i.State == EntityState.Modified || i.State == EntityState.Added);
+
+        foreach (var modifiedEntity in modifiedEntities)
+        {
+            var entity = (BaseEntity)modifiedEntity.Entity;
+
+            switch (modifiedEntity.State)
+            {
+                case EntityState.Added:
+                    entity.CreatedAt = DateTime.UtcNow;
+                    entity.UpdatedAt = DateTime.UtcNow; 
+                    break;
+
+                case EntityState.Modified:
+                    entity.UpdatedAt = DateTime.UtcNow;
+                    modifiedEntity.Property("CreatedAt").IsModified = false;
+                    break;
+            }
+        }
+
+        return base.SaveChanges();
+    }
 }
