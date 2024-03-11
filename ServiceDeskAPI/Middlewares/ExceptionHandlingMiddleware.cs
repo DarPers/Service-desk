c8582f97d2ec8646dea478453808a0c222805968
DarPers <dariaerohova21@gmail.com>
2024-03-11 11:52:07 +0100

    add exception handling middleware

---
+++ ServiceDeskAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using ServiceDeskAPI.ViewModels;
+using ServiceDesk.Domain.Exceptions;
+
+namespace ServiceDeskAPI.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var error = new ErrorModel
+        {
+            Message = exception.Message,
+            ExceptionType = exception.GetType().Name,
+            StackTrace = exception.StackTrace,
+            StatusCode = exception switch
+            {
+                EntityIsNullException => HttpStatusCode.NotFound,
+                _ => HttpStatusCode.InternalServerError,
+            }
+        };
+
+        _logger.LogError(error.ToString());
+
+        return context.Response.WriteAsJsonAsync(error);
+    }
+}
