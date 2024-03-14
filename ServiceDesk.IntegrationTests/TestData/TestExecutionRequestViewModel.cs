b2884fb75fec2b080cb8f1f5bc1f18983ebfcf5c
DarPers <dariaerohova21@gmail.com>
2024-03-14 08:26:09 +0100

    add integration tests for execution request controller
    (cherry picked from commit 789ff8c07d183970a18f2a2050326efa0ab1ebf3)

---
+++ ServiceDesk.IntegrationTests/TestData/TestExecutionRequestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ServiceDeskAPI.ViewModels;
+
+namespace ServiceDesk.IntegrationTests.TestData;
+public class TestExecutionRequestViewModel
+{
+    public static ExecutionRequestCreationViewModel CreateExecutionRequest(Guid userId, Guid ticketId) => new()
+    {
+        TicketId = ticketId,
+        ExecutorId = userId
+    };
+
+    public static ExecutionRequestViewModel CreateExecutionRequestViewModel(Guid userId, Guid ticketId) => new()
+    {
+        Id = Guid.NewGuid(),
+        TicketId = ticketId,
+        ExecutorId = userId
+    };
+}
