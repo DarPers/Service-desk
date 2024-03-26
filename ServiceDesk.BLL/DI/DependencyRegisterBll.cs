a3c861802713b58ac6ebb86757a42e89f427870b
DarPers <dariaerohova21@gmail.com>
2024-03-26 15:45:00 +0100

    add mass transit and rabbitMQ to DI

--- ServiceDesk.BLL/DI/DependencyRegisterBll.cs
+++ ServiceDesk.BLL/DI/DependencyRegisterBll.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceDesk.BLL.Interfaces;
 using ServiceDesk.BLL.Mapping;
@@ -8,7 +9,7 @@ using ServiceDesk.DAL.DI;
 namespace ServiceDesk.BLL.DI;
 public static class DependencyRegisterBll
 {
-    public static IServiceCollection AddBusinessLogicLevelServices(this IServiceCollection serviceCollection, 
+    public static IServiceCollection AddBusinessLogicLevelServices(this IServiceCollection serviceCollection,
         IConfiguration configuration)
     {
         serviceCollection.AddDataAccessLevelServices(configuration);
@@ -17,6 +18,21 @@ public static class DependencyRegisterBll
         serviceCollection.AddScoped<ITicketService, TicketService>();
         serviceCollection.AddScoped<IUserService, UserService>();
         serviceCollection.AddScoped<IExecutionRequestService, ExecutionRequestService>();
+        serviceCollection.AddScoped<IMessageService, MessageService>();
+
+        serviceCollection.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.UsingRabbitMq((context, busFactoryConfigurator) =>
+            {
+                busFactoryConfigurator.Host(configuration["RabbitMQ:HostName"], configuration["RabbitMQ:VirtualHostName"], configure =>
+                {
+                    configure.Username(configuration["RabbitMQ:UserName"]);
+                    configure.Password(configuration["RabbitMQ:Password"]);
+                });
+
+                busFactoryConfigurator.ConfigureEndpoints(context);
+            });
+        });
 
         return serviceCollection;
     }
