0f9b4f4c2388bd5650c9755eac97cb1eb54dcd71
DarPers <dariaerohova21@gmail.com>
2024-04-08 11:36:58 +0200

    modify device model

--- WarehouseAPI/Warehouse.DAL/Entities/Device.cs
+++ WarehouseAPI/Warehouse.DAL/Entities/Device.cs
@@ -7,15 +7,15 @@ public class Device
 {
     public Guid Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
 
-    public string SerialNumber { get; set; }
+    public string? SerialNumber { get; set; }
 
-    public string ModelName { get; set; }
+    public string? ModelName { get; set; }
 
-    public string Brand { get; set; }
+    public string? Brand { get; set; }
 
-    public string Characteristics { get; set; }
+    public string? Characteristics { get; set; }
 
     public Guid? UserId { get; set; }
 
